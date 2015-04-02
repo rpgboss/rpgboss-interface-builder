b7388037f866ac58a961cdb9f1f899a08d48c7f0
Hendrik <hendrik.weiler@gmx.de>
2015-04-02 16:01:24 +0200

    resizeable & draggable now working

--- src/js/app/controller/screen.js
+++ src/js/app/controller/screen.js
@@ -7,8 +7,32 @@ define([], function() {
 			$scope.$apply();
 		});
 
-		$scope.resize = function(evt) {
-			console.log(evt);
+		$rootScope.$on('element-drag', function(evt, data) {
+			console.log('drag',evt,data)
+			data.target.style.top = data.element.y + 'px';
+			data.target.style.left = data.element.x + 'px';
+		});
+
+		$rootScope.$on('element-resize', function(evt, data) {
+			console.log('resize',evt,data)
+			data.target.style.width = (data.element.width * data.element.scale) + 'px';
+			data.target.style.height = (data.element.height * data.element.scale) + 'px';
+		});
+
+		$scope.resize = function(evt, element) {
+			console.log(evt, element);
+			var width = $(evt.currentTarget).css('width');
+
+			element.scale = parseInt(width) / element.width;
+		}
+
+		$scope.drag = function(evt, element) {
+			console.log(evt, element);
+			var y = $(evt.currentTarget).css('top');
+			var x = $(evt.currentTarget).css('left');
+
+			element.x = parseInt(x);
+			element.y = parseInt(y);
 		}
 
 		$scope.imageLoaded = function(evt, element) {
@@ -16,6 +40,10 @@ define([], function() {
 			var height = evt.currentTarget.naturalHeight;
 			element.width = width;
 			element.height = height;
+
+			$rootScope.$broadcast('element-resize', {element:element, target:evt.currentTarget.parentNode});
+
+			evt.currentTarget.style.width = '100%';
 		}
 
 	}
--- src/js/app/directive/resizeable.js
+++ src/js/app/directive/resizeable.js
@@ -4,17 +4,33 @@ define([], function() {
         var resizableConfig = {
             aspectRatio: true
         };
+        var draggableConfig = {
+            containment : $('#screen')
+        };
 
         return {
             restrict: 'A',
             scope: {
-                callback: '&onResize'
+                onResize: '=onResize',
+                onDrag : '=onDrag',
+                model : '=model'
             },
             link: function postLink(scope, elem) {
                 elem.resizable(resizableConfig);
+                elem.draggable(draggableConfig);
+
                 elem.on('resizestop', function (evt) {
-                    if (scope.callback) scope.callback(evt);
+                    if (scope.onResize) scope.onResize(evt, scope.model);
                 });
+                elem.on( "dragstop", function(evt) {
+                    if (scope.onDrag) scope.onDrag(evt, scope.model);
+                });
+
+                setTimeout(function() {
+                    scope.$emit('element-resize',{element: scope.model, target : $(elem).get(0)});
+                    scope.$emit('element-drag',{element: scope.model, target : $(elem).get(0)});
+                },0);
+                
             }
         };
     }
